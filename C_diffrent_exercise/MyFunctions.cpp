fd1c85ac9d42f78209b8afc46c5afcd928d08947
BitCryptic <kanons.77.erstaunen@icloud.com>
2026-04-29 20:58:50 +0200

    Komplette Projektstruktur hinzugefügt

--- C_diffrent_exercise/MyFunctions.cpp
+++ C_diffrent_exercise/MyFunctions.cpp
@@ -1,3 +1,33 @@
-//
-// Created by narasi on 29.04.26.
-//
+#include <iostream>
+using namespace std;
+
+bool isPrime(long Zahl);
+
+int main() {
+    //************************
+    double Wert = 0.0;
+    int N;
+    cout << "Eingabe N = ";
+    cin >> N;
+    Wert = Fakultaet(N);
+    cout << endl << "Fakultaet von " << N << " = " << Wert << endl;
+
+
+    //***********************
+    long Testzahl = 0;
+    bool Ergebnis = false;
+
+    cout << "Hello World!\n";
+
+    cout << "Testzahl: ";
+    cin >> Testzahl;
+
+    Ergebnis = isPrime(Testzahl);       //Call by value
+    if (Ergebnis) {
+        cout << endl << "Die Zahl "<< Testzahl << " ist eine Primzahl!";
+    }
+    else {
+        cout << endl << "Die Zahl "<< Testzahl << " ist keine Primzahl!";
+    }
+    return 0;
+}
